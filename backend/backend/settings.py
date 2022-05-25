d0763e17f64c53b2b78b6ccccee8aa2956bfc60a
Elijah <ngfei25@gmail.com>
2022-05-25 15:32:23 +0800

    Clean up env

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)v(4pa-!-h6nb27d*gev@_)0g9xu#)hd$(uwkr_w)^08ym(nok'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
