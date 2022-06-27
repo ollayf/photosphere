f25ce58af508a5ee23ab941c76f59fc303dc9683
ollayf <ngfei25@gmail.com>
2022-06-27 15:51:32 +0800

    Clean release"

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('hello/', views.say_hello),
     path('usernameExists/', views.username_exists),
     path('addUser/', views.add_user),
     path('editUser/', views.edit_user),
