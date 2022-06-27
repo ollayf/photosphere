5fa748f248beef69a811d049f247107b7a34ad08
Chaitanya <gchaitanya@u.nus.edu>
2022-06-27 10:05:50 +0800

    Installing react-native-paper

--- frontend/src/Screens/AppMainPage.js
+++ frontend/src/Screens/AppMainPage.js
@@ -1,136 +1,78 @@
 import React, { useState } from 'react';
-import { SafeAreaView, View, StyleSheet } from 'react-native';
-import {  Button, Divider, Icon, Input, Layout, Text, TopNavigation, TopNavigationAction, BottomNavigation, BottomNavigationTab } from '@ui-kitten/components';
+import { SafeAreaView, StyleSheet } from 'react-native';
+import { Button, ApplicationProvider, IconRegistry, Divider, Icon, Layout, Input, TopNavigation, TopNavigationAction } from '@ui-kitten/components';
 import {AppLoading} from 'expo';
-import { ThemeContext } from '../../theme-context'
-import { NavigationContainer } from '@react-navigation/native'
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { ThemeContext } from '../../theme-context';
+import Smallerlogo from '../Components/Smallerlogo'
+import Passwordinput from '../Components/Passwordinput';
+import { Avatar, Title, Caption, Text, TouchabeRipple } from 'react-native-paper';
 
 
+const ProfileScreen = () => {
+  return (
+      <SafeAreaView style={styles.container}>
+      <TopNavigation title='My App' alignment='center'/>
+        <Divider/>
+        <View style = {styles.userInfoSection}>
+        <View> 
+          <Avatar.Image
+          source ={ { 
+            uri: 'https://api.adorable.io/avatar/80/abott@adorable.png'
+          }}
+          size = {80}
+          />
+        </View>
 
-export default function AppMainPage ({ navigation }) {
+     
+        
+          </View> 
+      </SafeAreaView> 
 
-  const { Navigator, Screen } = createBottomTabNavigator();
-
-
-  const navigateConvert = () => {
-    navigation.navigate('Convert');
-  };
-
-  const navigateExplore = () => {
-    navigation.navigate('Explore');
-  };
-
-  const navigateProfile = () => {
-    navigation.navigate('Profile');
-  };
-  const navigateAppMainPage = () => {
-    navigation.navigate('AppMainPage');
-  };
-
-
-
+  )
+}
   
-    const themeContext = React.useContext(ThemeContext);
-
-    const ProfileIcon = (props) => (
-      <Icon 
-      {...props} 
-      name='person-outline'
-      onSelect = {navigateProfile}
- 
-      />
-    );
-
-    const ConvertIcon = (props) => (
-      <Icon 
-      {...props} name='swap-outline'
-      onSelect = {navigateConvert}
-      />
-    );
-
-    const ExploreIcon = (props) => (
-      <Icon
-       {...props} 
-       name = 'compass-outline'
-       onSelect = {navigateExplore}
-       />
-    );
-
-    const ViewIcon = (props) => (
-      <Icon 
-      {...props} 
-      name='eye-outline'
-      onSelect = {navigateAppMainPage}
-      />
-    );
-
-    const useBottomNavigationState = (initialState = 0) => {
-      const [selectedIndex, setSelectedIndex] = React.useState(initialState);
-      return { selectedIndex, onSelect: setSelectedIndex };
-    };
-    const bottomState = useBottomNavigationState();
-    
-    
-    
-    return (
-        <SafeAreaView style={styles.container}>
-          <TopNavigation title='My App' alignment='center'/>
-          <Divider/>
-          <Layout style={styles.ChangeMode}>
-            <Button style={{ marginVertical: 4 }} onPress={themeContext.toggleTheme}>CHANGE MODE</Button>
-            
-            </Layout>
-             
-            
-            <Text 
-            category = 'h1'
-            style = {styles.header}
-            > View your 360 degree images </Text> 
-             
-            <BottomNavigation style={styles.bottomNavigation} {...bottomState}>
-            <BottomNavigationTab icon={ConvertIcon}/>
-            <BottomNavigationTab icon={ViewIcon}/>
-            <BottomNavigationTab icon={ExploreIcon}/>
-            <BottomNavigationTab icon={ProfileIcon}/>
+  
+  
           
-            </BottomNavigation>
-
-
-            
 
 
 
 
-          
-          </SafeAreaView>
-          )
-    }
+        
 
-    const styles = StyleSheet.create({
+const styles = StyleSheet.create({
 container: {
-  flex: 1,
-  
-  //justifyContent: 'center',
- // alignItems:'center'
- }, 
+flex: 1,
 
-ChangeMode: { 
-  flex: 0, 
-  flexDirection: 'row',
-  justifyContent: 'flex-end',
-  alignItems: 'center' ,
-},
-
-header: {
-  flex: 1, 
-  justifyContent: 'center',
-  alignItems: 'center',
-  fontSize: 20, 
-  
+//justifyContent: 'center',
+// alignItems:'center'
 }, 
 
-BottomNavigation: { 
-  marginVertical:8, 
+ChangeMode: { 
+flex: 0, 
+flexDirection: 'row',
+justifyContent: 'flex-end',
+alignItems: 'center' ,
 },
-    })
+
+userInfoSection: { 
+  paddingHorizontal: 30,
+  marginBottom: 25, 
+},
+
+title: { 
+  fontSize: 14, 
+  lineHeight: 14, 
+  fontWeight: '500', 
+}, 
+
+row:{ 
+  flexDirection: 'row',
+  marginBottom: 10,
+},
+
+infoBoxWrapper: {
+  borderBottomColor: '#ddddddd'
+}
+
+})
--- frontend/src/Screens/Profile.js
+++ frontend/src/Screens/Profile.js
@@ -1,69 +1,38 @@
 import React, { useState } from 'react';
 import { SafeAreaView, StyleSheet } from 'react-native';
-import { Button, ApplicationProvider, IconRegistry, Divider, Icon, Layout, Text, Input, TopNavigation, TopNavigationAction } from '@ui-kitten/components';
+import { Button, ApplicationProvider, IconRegistry, Divider, Icon, Layout, Input, TopNavigation, TopNavigationAction } from '@ui-kitten/components';
 import {AppLoading} from 'expo';
 import { ThemeContext } from '../../theme-context';
 import Smallerlogo from '../Components/Smallerlogo'
 import Passwordinput from '../Components/Passwordinput';
-import * as eva from '@eva-design/eva';
-
-import { Avatar } from '@ui-kitten/components';
+import { Avatar, Title, Caption, Text, TouchabeRipple } from 'react-native-paper';
 
 
-import { EvaIconsPack } from '@ui-kitten/eva-icons';
-
-const BackIcon = (props) => (
-  <Icon {...props} name='arrow-back' />
-);
-
-
-
-
-export default function ProfileScreen ({ navigation }) {
-
-
-  const [formData, setFormData] = useState({ 
-    name: '', 
-    email: ' ', 
-    password: '', 
-    //password2: ''
-  })
-  
-  const {name, email, password} = formData
-
-  
- // const onChange = (e) => {
-    //setFormData((prevState) => ({
-     // ...prevState, 
-   //   [e.target.name]: e.target.value, 
-   // }))
-
- // }
-  const themeContext = React.useContext(ThemeContext);
-
-  
-  
-  
+const ProfileScreen = () => {
   return (
       <SafeAreaView style={styles.container}>
-        <TopNavigation title='My App' alignment='center'/>
+      <TopNavigation title='My App' alignment='center'/>
         <Divider/>
-        <Layout style={styles.ChangeMode}>
-          <Button style={{ marginVertical: 4 }} onPress={themeContext.toggleTheme}>CHANGE MODE</Button>
-          
-          </Layout>
-           
-          
-           
-          <BottomNavigation style={styles.bottomNavigation} {...bottomState}>
-          <BottomNavigationTab icon={ConvertIcon}/>
-          <BottomNavigationTab icon={ViewIcon}/>
-          <BottomNavigationTab icon={ExploreIcon}/>
-          <BottomNavigationTab icon={ProfileIcon}/>
+        <View style = {styles.userInfoSection}>
+        <View> 
+          <Avatar.Image
+          source ={ { 
+            uri: 'https://api.adorable.io/avatar/80/abott@adorable.png'
+          }}
+          size = {80}
+          />
+        </View>
+
+     
         
-          </BottomNavigation>
+          </View> 
+      </SafeAreaView> 
 
-
+  )
+}
+  
+  
+  
           
 
 
